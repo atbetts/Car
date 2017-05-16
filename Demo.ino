6c87efce54b5aedf2ba8d168bf0382f74949c4d9
Andrew betts <abetts@ucsd.edu>
2017-05-15 17:00:45 -0700

    Add files via upload
    Base

---
+++ Demo.ino
@@ -0,0 +1,121 @@
+/*
+  LiquidCrystal Library - Autoscroll
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch demonstrates the use of the autoscroll()
+ and noAutoscroll() functions to make new text scroll or not.
+
+ The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalAutoscroll
+
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const byte numChars = 32;
+char receivedChars[numChars]; // an array to store the received data
+
+boolean newData = false;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  initialize(' ');
+  Serial.begin(9600);
+  Serial.println("<Arduino is ready>");
+  lcd.begin(16, 2);
+ 
+}
+
+void loop() {
+  // set the cursor to (0,0):
+  lcd.setCursor(0, 0);
+  // print from 0 to 9:
+  recvWithEndMarker();
+  showNewData();
+}
+void initialize(char a){
+   for (int thisChar = 0; thisChar < numChars; thisChar++) {
+    receivedChars[thisChar] = a;
+  }
+   
+  
+}
+void recvWithEndMarker() {
+ static byte ndx = 0;
+ char endMarker = '\n';
+ char rc;
+ 
+ // if (Serial.available() > 0) {
+           while (Serial.available() > 0 && newData == false) {
+ rc = Serial.read();
+
+ if (rc != endMarker) {
+ receivedChars[ndx] = rc;
+ ndx++;
+ if (ndx >= numChars) {
+ ndx = numChars - 1;
+ }
+ }
+ else {
+ receivedChars[ndx] = '\0'; // terminate the string
+ ndx = 0;
+ newData = true;
+ }
+
+ 
+ }
+ 
+}
+
+
+void showNewData(){
+if (newData == true) {
+   Serial.println(receivedChars);
+  lcd.clear();
+  
+  // set the display to automatically scroll:
+  lcd.setCursor(16, 1);
+  lcd.autoscroll();
+  for (int thisChar = 0; thisChar < 16; thisChar++) {
+    lcd.print(receivedChars[thisChar]);
+    receivedChars[thisChar] = ' ';
+    delay(500);
+  }
+  // set the cursor to (16,1):
+  
+  // turn off automatic scrolling
+  lcd.noAutoscroll();
+  newData = false;
+   }
+ 
+}
+
